270a2101532ff5d3d50ef146cf3ff5ac48077d0b
Chad Robinson <mr.chad.robinson@gmail.com>
2013-03-12 07:14:02 -0300

    Update ex18.py
    Oops.

--- ex18.py
+++ ex18.py
@@ -1,7 +1,7 @@
 # this one is like your scripts with argv
 def print_two(*args):
     arg1, arg2 = args
-    print "arg1: %, arg2: %r" % (arg1, arg2)
+    print "arg1: %r, arg2: %r" % (arg1, arg2)
 
 # ok, that *args is actually pointless, we can just do this
 def print_two_again(arg1, arg2):
